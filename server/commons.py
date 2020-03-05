7a59563a716fe20cf1a1f5d2f30f87489547ea65
Sofian Hadianto <sofian.hadianto@ninjavan.co>
2020-03-05 07:41:36 +0700

    Create decorator for main method, think it as an annotation for app launcher

---
+++ server/commons.py
@@ -0,0 +1,9 @@
+import inspect
+import sys
+
+def main(fn):
+    if inspect.stack()[1][0].f_locals['__name__'] == '__main__':
+        args = sys.argv[1:] # Discard the script name from command line
+        fn(*args) # Call the main function
+
+    return fn
