f98fdfc4c85f109d7e836ff1638f132838307950
Sofian Hadianto <sofian.hadianto@ninjavan.co>
2020-03-05 08:53:53 +0700

    Defining compose yml for orchestration and add small echo for debugging

--- client/AlphaClient.sh
+++ client/AlphaClient.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SERVER_NAME="node$NODE_NAME"
-
+echo "Node name = $SERVER_NAME"
 while inotifywait -q -e modify /var/log/messages > /dev/null
 do
     NUM_ATTEMPTS=$(grep -r "Accepted\|Failed" /var/log/messages | wc -l)
@@ -11,4 +11,9 @@ do
             '{server_name: $name, total_login_attempts: $cnt}')
 
     echo $REQUEST
+
+    curl --header "Content-Type: application/json" \
+	     --request POST \
+	     --data "${REQUEST}" \
+	     http://server/log
 done
