22df8293473c11824a6a4d2b2889072c6e4bb458
Sofian Hadianto <sofian.hadianto@ninjavan.co>
2020-03-05 07:58:26 +0700

    Defining data store and template index for main web landing

--- server/app.py
+++ server/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from commons import main
+from tinydb import TinyDB
 
 app = Flask(__name__)
+db = TinyDB('db.json')
 
 @app.route('/')
 def say_hay():
-    return 'Hello, world from AlphaServer'
+    data = db.all()
+    return render_template('index.html', data=data)
 
 @main
 def run():
