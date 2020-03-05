8739987b44a2b9e05f551c92944cdc84adfb6ccd
Sofian Hadianto <sofian.hadianto@ninjavan.co>
2020-03-05 08:03:07 +0700

    Defining post request. It will be used as an endpoint to insert log to db.json

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from commons import main
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 db = TinyDB('db.json')
@@ -9,6 +9,24 @@ db = TinyDB('db.json')
 def say_hay():
     data = db.all()
     return render_template('index.html', data=data)
+
+@app.route('/log', methods=['POST'])
+def recv_log_report():
+    data = request.json
+    server_name = data['server_name']
+    total_login_attempts = data['total_login_attempts']
+    
+    Log = Query()
+    res = db.search(Log.server_name == server_name)
+    
+    if len(res) > 0:
+        #update data
+        db.update({'total_login_attempts': total_login_attempts}, Log.server_name == server_name)
+    else:
+        #insert new data
+        db.insert(data)
+    
+    return 'ok'
 
 @main
 def run():
